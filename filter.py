f7435d126a2691dca345e296baae9d443bf56ec0
Shree Gurung -Nepal <shree.gurung@abacusinsights.com>
2024-04-11 17:39:23 +0545

    Code changed in filter.py

--- filter.py
+++ filter.py
@@ -1,5 +1,16 @@
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
+
+import snowflake.snowpark as snowpark
 from snowflake.snowpark.functions import col
 
-def filter_by_role(session, table_name, role):
-  df = session.table(table_name)
-  return df.filter(col("role") == role)
+def main(session: snowpark.Session): 
+    # Your code goes here, inside the "main" handler.
+    tableName = 'TASTY_BYTES_SAMPLE_DATA.RAW_POS.EMPLOYEES'
+    dataframe = session.table(tableName).filter(col("role") == "dev")
+
+    # Print a sample of the dataframe to standard output.
+    dataframe.show()
+
+    # Return value will appear in the Results tab.
+    return dataframe
